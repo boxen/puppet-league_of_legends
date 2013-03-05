2e2ad5b172f080e10ba4e21916a983a29380442e
petems <p.morsou@gmail.com>
2013-03-05 22:23:06 +0000

    Added example comment

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,8 @@
+# Public: Install League of Legends.app to /Applications.
+#
+# Examples
+#
+#   include league_of_legends
 class league_of_legends {
   package { 'League of Legends':
     source   => 'http://l3cdn.riotgames.com/Installer/EUW_Mac_Installer/League%20of%20Legends%20EUW.dmg',
