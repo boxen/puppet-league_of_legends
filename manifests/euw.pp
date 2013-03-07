7282a7094decb12b023cdbbf3dffdad6f9031a58
petems <p.morsou@gmail.com>
2013-03-07 20:43:27 +0000

    Fixed euw file

--- manifests/euw.pp
+++ manifests/euw.pp
@@ -3,7 +3,7 @@
 # Examples
 #
 #   include league_of_legends::euw
-class league_of_legends::eune inherits league_of_legends {
+class league_of_legends::euw inherits league_of_legends {
   Package['League of Legends']{
     source   => 'http://l3cdn.riotgames.com/Installer/EUW_Mac_Installer/League%20of%20Legends%20EUW.dmg',
   }
